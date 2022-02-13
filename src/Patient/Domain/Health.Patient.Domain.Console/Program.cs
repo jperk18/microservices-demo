35715d7b4c000060d6b13f35e459643861b60d07
jcper <jcperkins_18@hotmail.com>
2022-02-13 13:37:04 -0600

    changes for console app

--- src/Patient/Domain/Health.Patient.Domain.Console/Program.cs
+++ src/Patient/Domain/Health.Patient.Domain.Console/Program.cs
@@ -1,26 +1,38 @@
 using Health.Patient.Domain.Console.Core;
 using Health.Patient.Domain.Storage.Sql.Core;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using IHost = Microsoft.Extensions.Hosting.IHost;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Health.Patient.Domain.Console;
 
-// var preBuildConfig = new ConfigurationBuilder()
-//     .AddJsonFile("appsettings.json", optional: false)
-//     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-//     .AddEnvironmentVariables()
-//     .Build();
+static class Program
+{
+    static async Task Main(string[] args)
+    {
+        using IHost host = CreateHostBuilder(args).Build();
 
-// Add Configuration to the container
-builder.Configuration.AddJsonFile("appsettings.json", optional: false ,reloadOnChange: true);
-builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
-builder.Configuration.AddEnvironmentVariables();
+        // Application code should start here.
 
-// Add Services to Domain (and storage dependant service)
-var storageSettings = builder.Configuration.GetSection("DomainConfiguration:StorageConfiguration:PatientDatabase").Get<SqlDatabaseConfiguration>();
-builder.Services.AddDomainServices(new DomainConfiguration(new StorageConfiguration(storageSettings)));
+        await host.RunAsync();
+    }
 
-var app = builder.Build();
+    static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureHostConfiguration(configHost =>
+            {
+                var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-
-app.Run();
+                configHost.SetBasePath(Directory.GetCurrentDirectory());
+                configHost.AddJsonFile("appsettings.json", optional: false);
+                configHost.AddJsonFile($"appsettings.{environment}.json", optional: true);
+                configHost.AddEnvironmentVariables();
+            }).ConfigureServices((builder, services) =>
+            {
+                var storageSettings = builder.Configuration
+                    .GetSection("DomainConfiguration:StorageConfiguration:PatientDatabase")
+                    .Get<SqlDatabaseConfiguration>();
+                
+                services.AddDomainServices(new DomainConfiguration(new StorageConfiguration(storageSettings)));
+            });
+}
